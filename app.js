4aebd9c83c92159c6809836c9ed8716f7a65d498
jerin007 <jerinjahan@ymail.com>
2019-12-22 11:42:26 +0600

    Test

--- app.js
+++ app.js
@@ -35,11 +35,18 @@ app.use('/graphql', graphqlHttp({
 // 	res.json("Hello world");
 // });
 
-mongoose
-	.connect("mongodb+srv://testUser:mwIOUpRekyX0gjLv@node-graphql-hg7hv.mongodb.net/event-react-dev?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+// mongoose
+// 	.connect(`mongodb+srv://testUser:mwIOUpRekyX0gjLv@node-graphql-hg7hv.mongodb.net/event-react-dev?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+// 	.then(() => {
+// 		app.listen(PORT, console.log("server is running on ", PORT));
+// 	})
+// 	.catch(err => {
+// 		console.log(err);
+// 	});
+
+mongoose.connect(`mongodb+srv://siam:123456789SA@multisiam-pugea.mongodb.net/eventDB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
 		app.listen(PORT, console.log("server is running on ", PORT));
-	})
-	.catch(err => {
+	}).catch(err => {
 		console.log(err);
-	});
+	})
