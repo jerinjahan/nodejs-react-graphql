2049ee23751ca76d43e2f91f12feebb5de48b5ca
jerin007 <jerinjahan@ymail.com>
2019-12-22 11:56:25 +0600

    make it better

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const isAuth = require('./middleware/is-auth'); // Custom middleware for authori
 
 const app = express();
 const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@node-graphql-hg7hv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-const mongodb_URI = `mongodb+srv://testUser:mwIOUpRekyX0gjLv@node-graphql-hg7hv.mongodb.net/event-react-dev?retryWrites=true&w=majority`;
+const mongodb_URI = `mongodb+srv://jerin:987654321@node-graphql-hg7hv.mongodb.net/event-react-dev?retryWrites=true&w=majority`;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -30,23 +30,17 @@ app.use('/graphql', graphqlHttp({
 	graphiql: true
 }));
 
+
 // app.listen(PORT, console.log("server is running on ", PORT));
 // app.get('/', function (req, res) {
 // 	res.json("Hello world");
 // });
 
-// mongoose
-// 	.connect(`mongodb+srv://testUser:mwIOUpRekyX0gjLv@node-graphql-hg7hv.mongodb.net/event-react-dev?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true })
-// 	.then(() => {
-// 		app.listen(PORT, console.log("server is running on ", PORT));
-// 	})
-// 	.catch(err => {
-// 		console.log(err);
-// 	});
-
-mongoose.connect(`mongodb+srv://jerin:987654321@node-graphql-hg7hv.mongodb.net/event-react-dev?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+	.connect(mongodb_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
 	.then(() => {
 		app.listen(PORT, console.log("server is running on ", PORT));
-	}).catch(err => {
-		console.log(err);
 	})
+	.catch(err => {
+		console.log(err);
+	});
